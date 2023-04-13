cfa462e6a8bb91c61c651bcd925b268c2f0e8c5f
ZeCompadre <zecompadre@gmail.com>
2023-04-13 21:24:28 +0100

    ongoing

--- main.js
+++ main.js
@@ -36,16 +36,6 @@ app.controller('MapController', function ($scope, $timeout, $window, leafletBoun
 				},
 				onEachFeature: function (f, l) {
 					$scope.featureGroup.addLayer(l);
-					l.on('click', function (e) {
-						if ($scope.form.curId === null) {
-							if (selectedFeature) {
-								selectedFeature.editing.disable();
-								// and Here I'll add the code to store my edited polygon in the DB or whatever I want to do with it
-							}
-							selectedFeature = e.target;
-							e.target.editing.enable();
-						}
-					});
 				}
 			}
 		}
@@ -119,6 +109,12 @@ app.controller('MapController', function ($scope, $timeout, $window, leafletBoun
 	$scope.editFeature = function (feature) {
 		$scope.form.curId = feature.id;
 		$scope.form.text = feature.text;
+
+		if (selectedFeature) {
+			selectedFeature.editing.disable();
+		}
+		selectedFeature = feature;
+		feature.editing.enable();
 	};
 
 	$scope.updateFeature = function (text, zoom, id) {
