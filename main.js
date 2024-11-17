7fee445d44fe6bd9dc1f95722b343dca4cc4aa3a
ZeCompadre <zecompadre@gmail.com>
2024-11-17 16:37:40 +0000

    Update versioning for main.js and main.css; increment version query parameters in index.html and fix Snap interaction in addWKT function

--- main.js
+++ main.js
@@ -314,7 +314,7 @@ var app = (function () {
 		addWKT: async function () {
 			map.removeInteraction(select);
 			map.addInteraction(draw);
-			snap = new Snap({ sfeatures: features });
+			snap = new ol.interaction.Snap({ features: features });
 			map.addInteraction(snap);
 		},
 		copyWKT: async function () {
