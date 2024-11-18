214156fa7f5406414ae55a81a3bb5b607538488f
ZeCompadre <zecompadre@gmail.com>
2024-11-18 21:54:30 +0000

    Update version query parameters in index.html and fix MultiPolygon feature instantiation in main.js

--- main.js
+++ main.js
@@ -442,7 +442,7 @@ var app = (function () {
 						const multiPolygonGeometry = new ol.geom.MultiPolygon(polygonCoordinates);
 
 						// Optionally, create a new feature with the MultiPolygon geometry
-						const multiPolygonFeature = new Feature({
+						const multiPolygonFeature = new ol.Feature({
 							geometry: multiPolygonGeometry
 						});
 
