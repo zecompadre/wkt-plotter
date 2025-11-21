56c1cfbde375c2347af17b421275adc74ce180a2
ZeCompadre <zecompadre@gmail.com>
2025-11-21 19:45:59 +0000

    Refactor: Improve code formatting and consistency in App and mapUtilities modules

--- app.js
+++ app.js
@@ -5,29 +5,28 @@ import { SettingsManager } from './classes/SettingsManager.js';
 import { Translation } from './classes/Translation.js';
 import { TabSystem } from './classes/TabSystem.js';
 import { LightUI } from './classes/LightUI.js';
-import { Loading } from './classes/Loading.js';
 
 class App {
-    static async init() {
-        const { map, vectorLayer, loading } = setupMap();
+	static async init() {
+		const { map, vectorLayer, loading } = setupMap();
 
-        const translator = new Translation();
-        const settingsManager = new SettingsManager('settingsContainer', 'wkt-settings');
-        settingsManager.addEvent('wkt-presistent', 'change', (e) => {
-            if (e.target.checked) WKTUtilities.save();
-            else WKTUtilities.clear(false, true);
-        });
+		const translator = new Translation();
+		const settingsManager = new SettingsManager('settingsContainer', 'wkt-settings');
+		settingsManager.addEvent('wkt-presistent', 'change', (e) => {
+			if (e.target.checked) WKTUtilities.save();
+			else WKTUtilities.clear(false, true);
+		});
 
-        new LightUI();
-        const tabContainer = document.querySelector('#controls');
-        if (tabContainer) new TabSystem(tabContainer);
+		new LightUI();
+		const tabContainer = document.querySelector('#controls');
+		if (tabContainer) new TabSystem(tabContainer);
 
-        initializeControls(map, vectorLayer, translator, settingsManager);
+		initializeControls(map, vectorLayer, translator, settingsManager);
 
-        loading.show();
-        await mapUtilities.loadWKTs(true, false);
-        loading.hide();
-    }
+		loading.show();
+		await mapUtilities.loadWKTs(true, false);
+		loading.hide();
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => App.init());
